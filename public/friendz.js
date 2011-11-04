42542e9770bf08ea7c7a8fb172fbdfb6b3f96c14
ayalgelles <ayal.gelles@simplified-networks.com>
2011-11-04 16:06:22 +0200

    sdF

--- public/friendz.js
+++ public/friendz.js
@@ -1,16 +1,17 @@
-$('body').append($('<div id="friends"></div>'));
+$('body').append($('<div id="playit"></div>'));
 
 window.fbAsyncInit = function() {
+    
+    
     
   FB.init({ appId: cfg.appId, 
 	    status: true, 
 	    cookie: true,
 	    xfbml: true,
 	    oauth: true});
-    
-    
-    function getId(url){
-	var regex = /v[=\/]([\w|-]{11})/;
+
+    function getSince(url){
+	var regex = /since=(\d+)/;
 	try{
 	    var id = url.match(regex)[1]; // id = 'Ahg6qcgoay4'
 	    return id;
@@ -19,68 +20,99 @@ window.fbAsyncInit = function() {
 	    console.log('Error: ', url);
 	    return null;
 	}
+    }    
+
+    
+    function getId(url){
+	console.log(url);
+	var regex = /[=\/]([\w|-]{11})/;
+	try{
+	    var id = url.match(regex)[1]; // id = 'Ahg6qcgoay4'
+	    console.log('ID:', id);
+	    return id;
+
+	} catch (x) {
+	    console.log('Error: ', url);
+	    return null;
+	}
     }
 
-    function updateButton(response) {
+    alll = {};
+
+    $('#playit').click(function(){
+			   console.log(JSON.stringify(alll));
+			   console.log('http://3.bigrbox1.appspot.com/' + JSON.stringify(alll));
+		       });
+
+    function afterInit(response) {
 	var button = document.getElementById('fb-auth');
-	console.log(button);
+
 	if (response.authResponse) {
-	    FB.api('/me/friends', function(resp)
-	    {
-		var resp = {
-			   data: [{id: '100001083158634', name: 'gugugu'}]
-		       };
-		       
-		$(resp.data).each(function(i, frnd) {
-				      //					     var url = '/' + frnd.id + '/home';
-				      var url = '/me/home';
-//					     console.log(frnd.id);
-					     if (!window.location.hash || (window.location.hash && ('#' + frnd.id) === window.location.hash)){
 
-						 FB.api(url, function(fd){
-				//			    console.log(fd);
-//							    console.log(fd.data);
-							    if (fd.data) {
-								
-								$.each(fd.data, function(j, msg){
-									   var days = (new Date() - (new Date(msg.updated_time))) / 1000 / 60 / 60 / 24;
-									   // msg.comments && msg.comments.data && msg.comments.data.length > 2
-									   if (msg.link && msg.link.indexOf('youtube') > -1){
-									       var vid = getId(msg.link);
-				//					       console.log(vid);
-									       $('body').append(embedswf(vid, frnd.name, frnd.id, days));
-									       
-//									   console.log(frnd.name, j, msg.link);   
-									   }
-									   else if (msg.source && msg.source.indexOf('youtube') > -1){
-									       var vid = getId(msg.source);
-				//					       console.log(vid);
-									       $('body').append(embedswf(vid, frnd.name, frnd.id, days));
+	    (function iterateFeed(url){
+		 
+		 cb = function(fd) {
+			    if (fd.data) {
+			
+				console.log(fd);
+				$.each(fd.data, function(j, msg){
+					   console.log(msg.from.name);
 
-									       console.log('no youtube');
-									   }
-									   else {
-									       console.log('no youtube');
-									   }
-								       });
-							    }
-								  
-							    else {
-								       
-								console.log('no data', frnd.name, fd.data);
-							    }
-							
-						       });
-	 
-					     }
-				    });
-		   });
+					   var days = (new Date() - (new Date(msg.updated_time))) / 1000 / 60 / 60 / 24;
+					   
+					   var vid = null;
+					   if (msg.link && msg.link.indexOf('youtu') > -1){
+					       vid = getId(msg.link);    
+					   }
+					   if (msg.source && msg.source.indexOf('youtu') > -1){
+					       vid = getId(msg.source);    
+					   }
+					   
+					   if (vid) {
+					       all[vid] = {
+						   host: 'yt',
+						   url: 'http://phunnelz.com'
+					       };
+					   }
+				       });
 
+				if ($('#playlist').size() < 10){
+				    iterateFeed(fd.paging.next);
+				}
+			    }
+			    
+			    else {
+				
+				console.log('no data', 'gu', fd.data);
+			    }
+			    
+		 };
+
+		 if (!url){
+		     FB.api('/me/home', cb);
+		 } 
+		 else {
+		     console.log(url);
+		     $.ajax({
+				url: url,
+				dataType: 'jsonp',
+				jsonp: 'callback',
+				success: cb
+			    });
+        
+		 }
+
+	     })();
+	    
+
+
+	    
+	    
 	    //user is already logged in and connected
+	    $(button).text('Logout');
 	    button.onclick = function() {
 		FB.logout(function(response) {
-			      var userInfo = document.getElementById('user-info');
-			      userInfo.innerHTML="";
+			      button.text('Login');
 			  });
 	    };
 	} else {
@@ -98,8 +130,8 @@ window.fbAsyncInit = function() {
     }
 
   // run once with current status and whenever the status changes
-  FB.getLoginStatus(updateButton);
-//    FB.Event.subscribe('auth.statusChange', updateButton);
+    FB.getLoginStatus(afterInit);
+    FB.Event.subscribe('auth.statusChange', function(status){console.log(status);});
 };
 
 (function() {
